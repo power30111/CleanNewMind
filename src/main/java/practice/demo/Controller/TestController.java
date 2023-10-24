94d116c20307e17f8cace984819ccad77a62aa14
power30111_HOME <power30111@naver.com>
2023-10-24 18:08:48 +0900

    태초마을

---
+++ src/main/java/practice/demo/Controller/TestController.java
@@ -0,0 +1,22 @@
+package practice.demo.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class TestController {
+
+    @GetMapping("/test/api1")
+    @ResponseBody
+    public String testApi1(Model model){
+        log.info("check");
+        return "testApi1";
+    }
+
+}
