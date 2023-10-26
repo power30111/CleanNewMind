99b634620f687c45dfbad1af1785f8a22944989d
Wonwoo Park <gn030108@gmail.com>
2023-10-26 15:35:03 +0900

    1.스프링 리액트 테스트 1

--- src/main/java/practice/demo/Controller/TestController.java
+++ src/main/java/practice/demo/Controller/TestController.java
@@ -1,6 +1,5 @@
 package practice.demo.Controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
