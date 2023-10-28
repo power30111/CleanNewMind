a3116b0588fc5e22131428fda10fe7e0e06b7fb3
power30111_HOME <power30111@naver.com>
2023-10-28 17:31:38 +0900

    회원가입 요청 추가

--- src/main/java/practice/demo/Controller/UserController.java
+++ src/main/java/practice/demo/Controller/UserController.java
@@ -11,22 +11,33 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import practice.demo.Service.MemberService;
+import practice.demo.domain.Member;
 
 @Controller
 @RequestMapping("user")
 @Slf4j
+@CrossOrigin(origins = "http://localhost:3000") // 특정 출처만 허용
 public class UserController {
 
     @Autowired
     MemberService memberService;
-    
-    @CrossOrigin(origins = "http://localhost:3000") // 특정 출처만 허용
+
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestParam("id") String userid,
-                                    @RequestParam("password") String password){
-        memberService.LoginCheck(userid,password);
+    public ResponseEntity<String> loginUser(@RequestParam("id") String userId,
+                                            @RequestParam("password") String password){
+        memberService.LoginCheck(userId,password);
         log.info("로그인 요청들어옴.");
         return ResponseEntity.status(HttpStatus.OK).body("로그인 요청이 성공하였습니다.");
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestParam("id") String userId,
+                                               @RequestParam("password") String password,
+                                               @RequestParam("name") String userName,
+                                               @RequestParam("Email") String Email){
+        Member member = new Member(userId,password,userName,userName);
+        memberService.JoinMember(member);
+        return ResponseEntity.status(HttpStatus.OK).body("회원가입이 정상적으로 완료되었습니다.");
     }
 
 }
