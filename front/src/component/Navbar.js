15d4875f0eec311549f5a76cb98e1e88c695a208
Wonwoo Park <gn030108@gmail.com>
2023-11-02 16:58:31 +0900

    ui 만들기

--- front/src/component/Navbar.js
+++ front/src/component/Navbar.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav';
+import  {Button} from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 
 
 const Navbar = () => {
+
+  const navigate = useNavigate()
+
+    const goHome = () =>{
+        navigate('/')
+    }
+    const goLogin = () =>{
+        navigate('/login')
+    }
+    const goMembership = () =>{
+        navigate('/Membership')
+    }
+
   return (
-      <Nav
-      activeKey="/home"
-      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-    >
+      <Nav className="nav" activeKey="/home" onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
       <Nav.Item>
         <Nav.Link href="/home">Active</Nav.Link>
       </Nav.Item>
@@ -23,6 +35,9 @@ const Navbar = () => {
           Disabled
         </Nav.Link>
       </Nav.Item>
+      <Button variant="outline-primary" onClick={goHome}>Home</Button>
+      <Button variant="outline-primary" onClick={goLogin}>Login</Button>
+      <Button variant="outline-primary" onClick={goMembership}>Register</Button>
     </Nav>
   );
 /*    <div className='Navbar'>
