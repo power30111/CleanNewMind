16da1f49305d937a012540836bc6d08dfc341edc
Wonwoo Park <gn030108@gmail.com>
2023-11-12 17:29:40 +0900

    게시글 하나 조회 코드

--- front/src/component/List.js
+++ front/src/component/List.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch , useSelector } from 'react-redux'
+import axios from 'axios'
 
 
 
@@ -9,11 +10,39 @@ const List = (props) => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
+    const token = useSelector((state) => state.token)
+    const taketext = useSelector((state)=>state.taketext)
 
     const {id, title, writer} =props
 
+
     const detailPage = (()=>{
         navigate(`/board/list/${id}`)
+        dispatch({type:'urlid',payload:id})
+
+        axios.get(`http://localhost:8080/board/list/${id}`,{headers: {
+            Authorization: `Bearer ${token}` // Bearer 토큰 방식 사용
+        }})
+        .then((response) => {
+            if (response.status === 200) {
+                console.log("글 조회 성공")
+                console.log("글 수신",response)
+                dispatch({type:"takecontent",payload:response.data})
+                console.log("taketext",taketext)
+        }
+            else {
+            alert('글 조회 실패');
+            console.log("글 수신",response)
+
+            }
+        })
+        .catch(error => {
+            console.error('글 조회 실패 : ', error);
+            console.log("url아이디",id)
+
+        });
+
+
     })
 
     return (
