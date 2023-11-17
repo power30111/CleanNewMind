9577b570ec97ebb28ab27add724c3e456b1d9b13
Wonwoo Park <gn030108@gmail.com>
2023-11-17 16:23:56 +0900

    로그인 기능 수정, 글 수정시 초기화 추가

--- front/src/component/Navbar.js
+++ front/src/component/Navbar.js
@@ -27,15 +27,21 @@ const Navbar = () => {
     navigate('/Mypage')
   }
 
-    useEffect(()=>{
-        const logindata=localStorage.getItem("key");
-        if (logindata) {
-          dispatch({type:'logout', payload:false})
-        }
-        else {
-          dispatch({type:'login', payload:true})
-        }
-      },[]);
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      if (islogin) {
+        localStorage.removeItem('token');
+      }
+    };
+
+    // 이벤트 리스너 등록
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [islogin]); // islogin이 변경될 때만 useEffect 실행
   
 
 
