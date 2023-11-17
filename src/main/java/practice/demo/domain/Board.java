c85fd62d18e3fb9fb1c9081d2fdd771d8fe14bdb
power30111_HOME <power30111@naver.com>
2023-11-17 14:55:28 +0900

    수정기능 누락된거 올림

--- src/main/java/practice/demo/domain/Board.java
+++ src/main/java/practice/demo/domain/Board.java
@@ -29,4 +29,8 @@ public class Board extends BaseEntity{
         this.title = title;
         this.content = content;
     }
+    public void updateBoard(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 }
---
+++ src/main/java/practice/demo/exception/BoardNotFoundException.java
@@ -0,0 +1,7 @@
+package practice.demo.exception;
+
+public class BoardNotFoundException extends RuntimeException{
+    public BoardNotFoundException(String message) {
+        super(message);
+    }
+}
