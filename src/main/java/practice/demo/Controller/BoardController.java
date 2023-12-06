f137b941507eec903cfff1914aa7729265b0824b
power30111_HOME <power30111@naver.com>
2023-12-07 00:10:59 +0900

    검색할때 equal 이 아닌, like 로 조건을 걸도록 변경.

--- src/main/java/practice/demo/Controller/BoardController.java
+++ src/main/java/practice/demo/Controller/BoardController.java
@@ -49,7 +49,7 @@ public class BoardController {
 
     @GetMapping("/list/Page")
     public Page<BoardDto> searchBoard(BoardSearchCond cond, Pageable pageable){
-        log.info(cond.getBoardName()+"+"+cond.getWriterName()+" 의 조건으로 검색된 게시글들");
+        log.info(cond.getBn()+"+"+cond.getWn()+" 의 조건으로 검색된 게시글들");
         return boardService.searchPage(cond,pageable);
         /**
          * Pageable 관련.
